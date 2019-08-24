f9d985b062d6c44b3cefbbbf7866c7293244a520
wsq <wsq@wsqdeMacBook-Pro.local>
2019-08-24 16:47:30 +0800

    init

---
+++ main.ts
@@ -0,0 +1,16 @@
+declare const module: any;
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(1234);
+	console.log('Server is running on http://localhost:1234');
+	if (module.hot) {
+		module.hot.accept();
+		module.hot.dispose(() => app.close());
+	}
+}
+
+bootstrap();
