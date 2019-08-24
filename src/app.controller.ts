3566de2025c97c258b5dc0b5e7cb9d2947b8b977
wsq <wsq@wsqdeMacBook-Pro.local>
2019-08-24 21:37:20 +0800

    chore: return echostr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Query } from '@nestjs/common';
 import { IsWechat } from './isWechat.guard';
 
 @Controller()
 export class AppController {
 	@Get('')
 	@UseGuards(IsWechat)
-	hello() {
-		return 'hello!!';
+	hello(@Query() { echostr }) {
+		return echostr;
 	}
 }
